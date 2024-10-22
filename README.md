cba16077f7b93948397e956ab9dfbc405db8a0c7
Isabela França <fysabelah@gmail.com>
2024-10-22 16:01:44 -0300

    - Normalizar a operação de porcentagem - Atualizar o README.md

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Calculadora
- Uma calculadora que realize as operações de soma, subtração, multiplicação, divisão e resto, fazendo uso de HTML, CSS e JavaScript.
+
+Uma calculadora, criada com CSS, HTML e JS, capaz de realizar operações de:
+
+* Soma
+* Subtração
+* Divisão
+* Multiplicação
+* Raiz Quadrada
+* Porcentagem
+
+A expressão irá aceitar mais de uma operação e também validará se o operador pode ou não ser utilizado, por exemplo, uma operação
+deve começar com número ou raiz quadrada e caso o requisito não seja atendido, o usuário será alertado.
+
+<img src="./calculator.gif">
--- calculator-script.js
+++ calculator-script.js
@@ -4,7 +4,7 @@ function addListenerOnButtons() {
     buttons.forEach(button => {
         let buttonTextValue = button.textContent;
 
-        if (buttonTextValue == '='){
+        if (buttonTextValue === '=') {
             button.addEventListener('click', () => getResult());
         } else {
             button.addEventListener('click', () => putValueOnDisplay(buttonTextValue));
@@ -21,7 +21,7 @@ function getResult() {
 
         const result = calculateExpression(expression);
 
-        if (result){
+        if (result) {
             display.innerHTML = result % 1 === 0 ? result : result.toFixed(3);
         }
     }
@@ -41,11 +41,11 @@ function normalizeMultiplicationAndDivision(expression) {
 
 function normalizeSquareRoot(tokens, operators) {
     let sqrtIndex = tokens.indexOf('√');
-    
-    while(sqrtIndex != -1) {
+
+    while (sqrtIndex !== -1) {
         tokens[sqrtIndex] = 'Math.sqrt(';
-    
-        for (let i = sqrtIndex + 1; i < tokens.length; i ++) {
+
+        for (let i = sqrtIndex + 1; i < tokens.length; i++) {
             if (!operators.includes(tokens[i])) {
                 tokens[i] += ')';
                 break;
@@ -67,13 +67,39 @@ function normalizeExpression(expression) {
 
     tokens = normalizeSquareRoot(tokens, operators);
 
-    tokens = normalizePercentage(tokens, operators);
+    tokens = normalizePercentage(tokens);
 
     return tokens.join(" ");
 }
 
-function normalizePercentage(tokens, operators){
-    // todo falta implementar
+function normalizePercentage(tokens) {
+    let percentIndex = tokens.indexOf('%');
+
+    if (percentIndex === 1 && tokens.length === 2) {
+        tokens.pop();
+        tokens[0] /= 100;
+
+        return tokens;
+    }
+
+    while (percentIndex !== -1) {
+        let previousToken = percentIndex - 2;
+
+        if (previousToken < 0) {
+            tokens.splice(percentIndex, 1);
+            tokens[0] /= 100;
+        } else {
+            if (['/', '*'].includes(tokens[percentIndex - 2])) {
+                tokens[percentIndex - 1] /= 100;
+            } else {
+                tokens[percentIndex - 1] = tokens[percentIndex - 3] * tokens[percentIndex - 1] / 100;
+            }
+
+            tokens.splice(percentIndex, 1);
+        }
+
+        percentIndex = -1;
+    }
 
     return tokens;
 }
@@ -84,15 +110,15 @@ function putValueOnDisplay(textContent) {
     const operators = ['√', '%', '×', '÷', '-', '+'];
     let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     let clear = ["C", "CE"];
-    
+
     if (clear.includes(textContent)) {
-        clearOrDeleteLastCaracter(textContent, display, expression);
+        clearOrDeleteLastCharacter(textContent, display, expression);
     } else {
-        if (expression.length == 0 && !numbers.includes(textContent) && textContent != '√') {
+        if (expression.length === 0 && !numbers.includes(textContent) && textContent !== '√') {
             alert('A expressão deve começar com um número ou raiz quadrada!');
         } else if (expression.length >= 8) {
             alert('O tamanho máximo para expressão é de 8 caracteres!');
-        } else if (textContent == '.') {
+        } else if (textContent === '.') {
             addDotToExpression(expression, display, operators);
         } else if (numbers.includes(textContent)) {
             addNumberToExpression(textContent, display, expression);
@@ -106,25 +132,25 @@ function addDotToExpression(expression, displaySelector, operators) {
     const tokens = expression.match(/(\d+(\.\d+)?|\D)/g);
     const lastValueOnExpression = tokens[tokens.length - 1];
 
-    if (!operators.includes(lastValueOnExpression) && expression.length <= 6 && lastValueOnExpression.search('\\.') == -1) {
+    if (!operators.includes(lastValueOnExpression) && expression.length <= 6 && lastValueOnExpression.search('\\.') === -1) {
         displaySelector.textContent += '.';
     } else {
         alert('O . só pode ser adicionado após um número e em expressão de no máximo 6 caracteres!');
     }
 }
 
-function addNumberToExpression(textContent, displaySelector, expression){
+function addNumberToExpression(textContent, displaySelector, expression) {
     let lastValueOnExpression = expression[expression.length - 1];
 
-    if (lastValueOnExpression == '%') {
+    if (lastValueOnExpression === '%') {
         alert('Após % deve haver outra operação!');
     } else {
         displaySelector.textContent += textContent;
     }
 }
 
-function clearOrDeleteLastCaracter(textContent, displaySelector, expression) {
-    if (textContent == 'CE') {
+function clearOrDeleteLastCharacter(textContent, displaySelector, expression) {
+    if (textContent === 'CE') {
         displaySelector.textContent = expression.substring(0, expression.length - 1);
     } else {
         displaySelector.textContent = ''
